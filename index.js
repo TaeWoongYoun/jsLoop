69a953308e1127333c2f61e0c7342cc96c222c57
xodnd0507 <24457545yong@gmail.com>
2024-02-24 22:11:09 +0900

    test : Used var

--- index.js
+++ index.js
@@ -4,23 +4,26 @@
 // - 박스0,1,2에 붙어있던 show 클래스명 전부 제거하라고 코드 3줄 짜기
 // - 박스0에 show 클래스명 부착
 
+var 제거 = $('.tab-button').removeClass('orange');
+var 삭제 = $('.tab-content').removeClass('show');
+
 $('.tab-button').eq(0).on('click', function(){
-    $('.tab-button').removeClass('orange');
+    제거
     $('.tab-button').eq(0).addClass('orange');
-    $('.tab-content').removeClass('show');
+    삭제
     $('.tab-content').eq(0).addClass('show');
 })
 
 $('.tab-button').eq(1).on('click', function(){
-    $('.tab-button').removeClass('orange');
+    제거
     $('.tab-button').eq(1).addClass('orange');
-    $('.tab-content').removeClass('show');
+    삭제
     $('.tab-content').eq(1).addClass('show');
 })
 
 $('.tab-button').eq(2).on('click', function(){
-    $('.tab-button').removeClass('orange');
+    제거
     $('.tab-button').eq(2).addClass('orange');
-    $('.tab-content').removeClass('show');
+    삭제
     $('.tab-content').eq(2).addClass('show');
 })
