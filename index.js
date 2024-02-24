0093a5f76e84a086978f753e3503c0a8c4f7d9c4
xodnd0507 <24457545yong@gmail.com>
2024-02-24 22:30:48 +0900

    feat : Use for tabmenu

--- index.js
+++ index.js
@@ -4,29 +4,29 @@
 // - 박스0,1,2에 붙어있던 show 클래스명 전부 제거하라고 코드 3줄 짜기
 // - 박스0에 show 클래스명 부착
 
-for(var i = 0; i < 3; i++){
-    console.log('안녕');
+for(let i = 0; i < 4; i++){
+
+$('.tab-button').eq(i).on('click', function(){
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(i).addClass('orange');
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(i).addClass('show');
+})
 }
 
 
 
-$('.tab-button').eq(0).on('click', function(){
-    $('.tab-button').removeClass('orange');
-    $('.tab-button').eq(0).addClass('orange');
-    $('.tab-content').removeClass('show');
-    $('.tab-content').eq(0).addClass('show');
-})
 
-$('.tab-button').eq(1).on('click', function(){
-    $('.tab-button').removeClass('orange');
-    $('.tab-button').eq(1).addClass('orange');
-    $('.tab-content').removeClass('show');
-    $('.tab-content').eq(1).addClass('show');
-})
+// $('.tab-button').eq(1).on('click', function(){
+//     $('.tab-button').removeClass('orange');
+//     $('.tab-button').eq(1).addClass('orange');
+//     $('.tab-content').removeClass('show');
+//     $('.tab-content').eq(1).addClass('show');
+// })
 
-$('.tab-button').eq(2).on('click', function(){
-    $('.tab-button').removeClass('orange');
-    $('.tab-button').eq(2).addClass('orange');
-    $('.tab-content').removeClass('show');
-    $('.tab-content').eq(2).addClass('show');
-})
+// $('.tab-button').eq(2).on('click', function(){
+//     $('.tab-button').removeClass('orange');
+//     $('.tab-button').eq(2).addClass('orange');
+//     $('.tab-content').removeClass('show');
+//     $('.tab-content').eq(2).addClass('show');
+// })
